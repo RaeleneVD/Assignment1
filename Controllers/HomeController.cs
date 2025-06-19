fd8aca30569fc84999b758e50d5ad2e0955b8451
RaeleneV <raelenevdookkoo@gmail.com>
2025-06-19 19:53:44 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SelectSerivice()
+        {
+            ViewBag.Message = "Select Service page.";
+
+            return View();
+        }
+
+        public ActionResult BookingForm(string service)
+        {
+            ViewBag.service = service;
+
+            return View();
+        }
+
+        public ActionResult BookingConfirmed()
+        {
+            return View();
+
+        }
+
+        public ActionResult RideHistory()
+        {
+            return View();
+
+        }
+
+        public ActionResult Manage()
+        {
+            return View();
+
+        }
+    }
+}
